cd3d7d3ae54eb05c069f9af964508c6aff943b12
Jessica John <jjjohn@fullsail.edu>
2017-04-05 10:00:39 -0400

    Added homepage markup, created the register form component, created validation service and implemented angular2 flash messages module

--- meanauthapp/angular-src/src/app/components/register/register.component.ts
+++ meanauthapp/angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+//import { FlashMessagesService } from 'angular2-flash-messages'; 
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+   name: String;
+   username: String;
+   email: String;
+   password: String;
 
-  constructor() { }
+  constructor(private vaidateService: ValidateService, /*public flashMessage: FlashMessagesService*/) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+    //Required Fields
+   if(!this.vaidateService.validateRegister(user)){
+      //this.flashMessage.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      console.log('Please fill in all fields');
+      return false;
+   }
+
+   //Email Validate
+    if(!this.vaidateService.validateEmail(user.email)){
+      //this.flashMessage.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+      console.log('Please use a valid email');
+      return false;
+   }
   }
 
 }
